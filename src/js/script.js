4581417c8b1c13c42e25a78a7b901aa14774e77f
IsaKegin <85554066+IsaKegin@users.noreply.github.com>
2021-07-30 18:47:37 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
-function(){
-    
+"use strict"
+
+function toggleMobileMenu(obj1, obj2) {
+    if (obj1.className == "ul")
+    {
+        obj1.className = "ul menu_hide";
+        obj2.className = "close_menu menu_hide";
+    }
+    else
+    {
+        obj1.className = "ul";
+        obj2.className = "close_menu";
+    }
+}
+
+function hideMobileMenu(obj1, obj2){
+    obj1.className = "ul menu_hide";
+    obj2.className = "close_menu menu_hide";
 }
